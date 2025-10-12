db04fd8ec1f301e0d71dc6c39ca6150b990c274d
kranoley <127663416+kranoley@users.noreply.github.com>
2025-10-12 11:24:27 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import customtkinter as ctk
+from ui.app import YouTubeAnalyzerApp
+
+if __name__ == "__main__":
+    ctk.set_appearance_mode("dark")
+    app = YouTubeAnalyzerApp()
+    app.mainloop()
